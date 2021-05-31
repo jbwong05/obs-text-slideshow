e348dc1dc8c5bdfb0a50dae4a022135f73826559
Josh Wong <jbwong05@gmail.com>
2021-05-30 23:44:24 -0700

    Implemented getname

--- src/obs-text-slideshow.c
+++ src/obs-text-slideshow.c
@@ -1,12 +1,37 @@
+/*
+obs-text-slideshow
+Copyright (C) 2021 Joshua Wong jbwong05@gmail.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>
+*/
+
 #include <obs-module.h>
+
+static const char *text_ss_getname(void *unused) {
+    UNUSED_PARAMETER(unused);
+    return obs_module_text("TextSlideshow");
+}
+
+
 
 struct obs_source_info text_slideshow_info = {
 	.id = "text-slideshow",
 	.type = OBS_SOURCE_TYPE_INPUT,
 	.output_flags = OBS_SOURCE_VIDEO | OBS_SOURCE_CUSTOM_DRAW |
 			OBS_SOURCE_COMPOSITE | OBS_SOURCE_CONTROLLABLE_MEDIA,
-	/*.get_name = ss_getname,
-	.create = ss_create,
+	.get_name = text_ss_getname,
+	/*.create = ss_create,
 	.destroy = ss_destroy,
 	.update = ss_update,
 	.activate = ss_activate,
